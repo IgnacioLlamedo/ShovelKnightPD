abb09967fe7d3b1533b5daeb9491aba43eb0a1bc
Ignacio Llamedo <ignaciollamedo@hotmail.com>
2023-07-17 12:06:23 -0300

    hud y eventos con el teclado

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 let i = Number;
 const arrayX = [];
 const arrayY = [];
-const arrayBloques = [];
+const arrayEntidad = [];
 const bloques = document.getElementById('bloques');
 let posV = 90;
-let posH = 195;
+let posH = 360;
 let tileY = 1;
 let tileX = 1;
 let mejorar = document.getElementById("mejorar");
+let hud = document.getElementById("hud");
 const girarPersonaje = document.querySelector('#personaje');
+let vidaPersonaje = 5;
 class Entidad{
-    constructor(vida, puntos, x, y, imagen, ataque, id){
+    constructor(vida, puntos, x, y, imagen, ataque, num){
         this.vida = vida;
         this.puntos = puntos;
         this.x = x;
         this.y = y;
         this.imagen = imagen;
         this.ataque = ataque;
-        this.id = id;
+        this.num = num;
     }
 }
 //Puntos en el storage
@@ -31,43 +33,66 @@ class Entidad{
     }
     const puntosDiv = document.querySelector('#puntos span');
     puntosDiv.innerText = puntos;
+//Vida en el storage
+    let vidaJSON = localStorage.getItem("vidaPersonaje");
+    if(vidaJSON != null){
+        vidaPersonaje = vidaJSON;
+    }
 //Mejora en el storage
     let palaMejorada = false
     let mejoraJSON = localStorage.getItem("palaMejorada");
     if (mejoraJSON === "true"){
         mejorar.remove();
         palaMejorada = true
+        hud.innerHTML =`
+        <img src="img/hud2_${vidaPersonaje}.webp">`
     }
     else{
         palaMejorada = false
+        hud.innerHTML =`
+        <img src="img/hud1_${vidaPersonaje}.webp">`
     }
 //Función para mover al personaje hacia arriba
     function u(){
         if(posV > 90){
-            let hayBloque = false;
-            let cualBloque = -1;
-            for (i = 0; i < arrayBloques.length; i++){
-                if ((arrayBloques[i].y == tileY - 1) & (arrayBloques[i].x == tileX)){
-                    hayBloque = true;
-                    cualBloque = i;
+            let hayEntidad = false;
+            let cualEntidad = -1;
+            for (i = 0; i < arrayEntidad.length; i++){
+                if ((arrayEntidad[i].y == tileY - 1) & (arrayEntidad[i].x == tileX)){
+                    hayEntidad = true;
+                    cualEntidad = i;
                 }
             }
 //Si hay bloque, ataca
-            if ((hayBloque == true) & (cualBloque != -1)){
+            if ((hayEntidad == true) & (cualEntidad != -1)){
                 if (palaMejorada == true){
-                    arrayBloques[cualBloque].vida -= 3;
+                    arrayEntidad[cualEntidad].vida -= 3;
                 }
                 else{
-                    arrayBloques[cualBloque].vida -= 1;
+                    arrayEntidad[cualEntidad].vida -= 1;
                 }
-                if (arrayBloques[cualBloque].vida <= 0){
-                    let bloque = document.getElementById(`b${arrayBloques[cualBloque].id}`);
+                if (arrayEntidad[cualEntidad].vida <= 0){
+                    let bloque = document.getElementById(`b${arrayEntidad[cualEntidad].num}`);
                     bloque.remove();
-                    puntos = puntos + arrayBloques[cualBloque].puntos;
+                    puntos = puntos + arrayEntidad[cualEntidad].puntos;
                     localStorage.setItem("puntos", puntos);
                     puntosDiv.innerText = puntos;
-                    arrayBloques.splice(cualBloque, 1);
-                    console.log(arrayBloques);
+                    arrayEntidad.splice(cualEntidad, 1);
+                }
+                else if (arrayEntidad[cualEntidad].vida > 0){
+                    vidaPersonaje = vidaPersonaje - (arrayEntidad[cualEntidad].ataque);
+                    if (vidaPersonaje > 5){
+                        vidaPersonaje = 5;
+                    }
+                    if(palaMejorada == true){
+                        hud.innerHTML =`
+                        <img src="img/hud2_${vidaPersonaje}.webp">`
+                    }
+                    else if(palaMejorada == false){
+                        hud.innerHTML =`
+                        <img src="img/hud1_${vidaPersonaje}.webp">`
+                    }
+                    localStorage.setItem("vidaPersonaje", vidaPersonaje);
                 }
             }
 //Si no hay bloque, se mueve
@@ -76,38 +101,50 @@ class Entidad{
                 posV = posV - 60;
                 personaje.style.top = posV + "px";
                 tileY = tileY - 1;
-                console.log(tileX)
-                console.log(tileY)
             }
         }
     }
     function d(){
         if(posV < 440){
-            let hayBloque = false;
-            let cualBloque = -1;
-            for (i = 0; i < arrayBloques.length; i++){
-                if ((arrayBloques[i].y == tileY + 1) & (arrayBloques[i].x == tileX)){
-                    hayBloque = true;
-                    cualBloque = i;
+            let hayEntidad = false;
+            let cualEntidad = -1;
+            for (i = 0; i < arrayEntidad.length; i++){
+                if ((arrayEntidad[i].y == tileY + 1) & (arrayEntidad[i].x == tileX)){
+                    hayEntidad = true;
+                    cualEntidad = i;
                 }
             }
 //Si hay bloque, ataca
-            if ((hayBloque == true) & (cualBloque != -1)){
+            if ((hayEntidad == true) & (cualEntidad != -1)){
                 if (palaMejorada == true){
-                    arrayBloques[cualBloque].vida -= 3;
+                    arrayEntidad[cualEntidad].vida -= 3;
                 }
                 else{
-                    arrayBloques[cualBloque].vida -= 1;
+                    arrayEntidad[cualEntidad].vida -= 1;
                 }
-                if (arrayBloques[cualBloque].vida <= 0){
-                    let bloque = document.getElementById(`b${arrayBloques[cualBloque].id}`);
+                if (arrayEntidad[cualEntidad].vida <= 0){
+                    let bloque = document.getElementById(`b${arrayEntidad[cualEntidad].num}`);
                     bloque.remove();
-                    puntos = puntos + arrayBloques[cualBloque].puntos;
+                    puntos = puntos + arrayEntidad[cualEntidad].puntos;
                     localStorage.setItem("puntos", puntos);
                     puntosDiv.innerText = puntos;
-                    arrayBloques.splice(cualBloque, 1);
-                    console.log(arrayBloques);
+                    arrayEntidad.splice(cualEntidad, 1);
                 }
+                else if (arrayEntidad[cualEntidad].vida > 0){
+                    vidaPersonaje = vidaPersonaje - (arrayEntidad[cualEntidad].ataque);
+                    if (vidaPersonaje > 5){
+                        vidaPersonaje = 5;
+                    }
+                    if(palaMejorada == true){
+                        hud.innerHTML =`
+                        <img src="img/hud2_${vidaPersonaje}.webp">`
+                    }
+                    else if(palaMejorada == false){
+                        hud.innerHTML =`
+                        <img src="img/hud1_${vidaPersonaje}.webp">`
+                    }
+                }
+                localStorage.setItem("vidaPersonaje", vidaPersonaje);
             }
 //Si no hay bloque, se mueve
             else{
@@ -115,40 +152,52 @@ class Entidad{
                 posV = posV + 60;
                 personaje.style.top = posV + "px";
                 tileY = tileY + 1;
-                console.log(tileX)
-                console.log(tileY)
             }
         }
     }
     function l(){
         girarPersonaje.innerHTML = `
-        <img src="img/shovel_knightL.png" alt="" class="img">`
-        if(posH > 195){
-            let hayBloque = false;
-            let cualBloque = -1;
-            for (i = 0; i < arrayBloques.length; i++){
-                if ((arrayBloques[i].x == tileX - 1) & (arrayBloques[i].y == tileY)){
-                    hayBloque = true;
-                    cualBloque = i;
+        <img src="img/shovel_knightL.webp" alt="" class="img">`
+        if(posH > 360){
+            let hayEntidad = false;
+            let cualEntidad = -1;
+            for (i = 0; i < arrayEntidad.length; i++){
+                if ((arrayEntidad[i].x == tileX - 1) & (arrayEntidad[i].y == tileY)){
+                    hayEntidad = true;
+                    cualEntidad = i;
                 }
             }
 //Si hay bloque, ataca
-            if ((hayBloque == true) & (cualBloque != -1)){
+            if ((hayEntidad == true) & (cualEntidad != -1)){
                 if (palaMejorada == true){
-                    arrayBloques[cualBloque].vida -= 3;
+                    arrayEntidad[cualEntidad].vida -= 3;
                 }
                 else{
-                    arrayBloques[cualBloque].vida -= 1;
+                    arrayEntidad[cualEntidad].vida -= 1;
                 }
-                if (arrayBloques[cualBloque].vida <= 0){
-                    let bloque = document.getElementById(`b${arrayBloques[cualBloque].id}`);
+                if (arrayEntidad[cualEntidad].vida <= 0){
+                    let bloque = document.getElementById(`b${arrayEntidad[cualEntidad].num}`);
                     bloque.remove();
-                    puntos = puntos + arrayBloques[cualBloque].puntos;
+                    puntos = puntos + arrayEntidad[cualEntidad].puntos;
                     localStorage.setItem("puntos", puntos);
                     puntosDiv.innerText = puntos;
-                    arrayBloques.splice(cualBloque, 1);
-                    console.log(arrayBloques);
+                    arrayEntidad.splice(cualEntidad, 1);
                 }
+                else if (arrayEntidad[cualEntidad].vida > 0){
+                    vidaPersonaje = vidaPersonaje - (arrayEntidad[cualEntidad].ataque);
+                    if (vidaPersonaje > 5){
+                        vidaPersonaje = 5;
+                    }
+                    if(palaMejorada == true){
+                        hud.innerHTML =`
+                        <img src="img/hud2_${vidaPersonaje}.webp">`
+                    }
+                    else if(palaMejorada == false){
+                        hud.innerHTML =`
+                        <img src="img/hud1_${vidaPersonaje}.webp">`
+                    }
+                }
+                localStorage.setItem("vidaPersonaje", vidaPersonaje);
             }
 //Si no hay bloque, se mueve
             else{
@@ -156,40 +205,52 @@ class Entidad{
                 posH = posH - 60;
                 personaje.style.left = posH + "px";
                 tileX = tileX - 1;
-                console.log(tileX)
-                console.log(tileY)
             }
         }
     }
     function r(){
         girarPersonaje.innerHTML =`
-        <img src="img/shovel_knightR.png" alt="" class="img">`
-        if(posH < 550){
-            let hayBloque = false;
-            let cualBloque = -1;
-            for (i = 0; i < arrayBloques.length; i++){
-                if ((arrayBloques[i].x == tileX + 1) & (arrayBloques[i].y == tileY)){
-                    hayBloque = true;
-                    cualBloque = i;
+        <img src="img/shovel_knightR.webp" alt="" class="img">`
+        if(posH < 715){
+            let hayEntidad = false;
+            let cualEntidad = -1;
+            for (i = 0; i < arrayEntidad.length; i++){
+                if ((arrayEntidad[i].x == tileX + 1) & (arrayEntidad[i].y == tileY)){
+                    hayEntidad = true;
+                    cualEntidad = i;
                 }
             }
 //Si hay bloque, ataca
-            if ((hayBloque == true) & (cualBloque != -1)){
+            if ((hayEntidad == true) & (cualEntidad != -1)){
                 if (palaMejorada == true){
-                    arrayBloques[cualBloque].vida -= 3;
+                    arrayEntidad[cualEntidad].vida -= 3;
                 }
                 else{
-                    arrayBloques[cualBloque].vida -= 1;
+                    arrayEntidad[cualEntidad].vida -= 1;
                 }
-                if (arrayBloques[cualBloque].vida <= 0){
-                    let bloque = document.getElementById(`b${arrayBloques[cualBloque].id}`);
+                if (arrayEntidad[cualEntidad].vida <= 0){
+                    let bloque = document.getElementById(`b${arrayEntidad[cualEntidad].num}`);
                     bloque.remove();
-                    puntos = puntos + arrayBloques[cualBloque].puntos;
+                    puntos = puntos + arrayEntidad[cualEntidad].puntos;
                     localStorage.setItem("puntos", puntos);
                     puntosDiv.innerText = puntos;
-                    arrayBloques.splice(cualBloque, 1);
-                    console.log(arrayBloques);
+                    arrayEntidad.splice(cualEntidad, 1);
                 }
+                else if (arrayEntidad[cualEntidad].vida > 0){
+                    vidaPersonaje = vidaPersonaje - (arrayEntidad[cualEntidad].ataque);
+                    if (vidaPersonaje > 5){
+                        vidaPersonaje = 5;
+                    }
+                    if(palaMejorada == true){
+                        hud.innerHTML =`
+                        <img src="img/hud2_${vidaPersonaje}.webp">`
+                    }
+                    else if(palaMejorada == false){
+                        hud.innerHTML =`
+                        <img src="img/hud1_${vidaPersonaje}.webp">`
+                    }
+                }
+                localStorage.setItem("vidaPersonaje", vidaPersonaje);
             }
 //Si no hay bloque, se mueve
             else{
@@ -197,8 +258,6 @@ class Entidad{
                 posH = posH + 60;
                 personaje.style.left = posH + "px";
                 tileX = tileX + 1;
-                console.log(tileX)
-                console.log(tileY)
             }
         }
     }
@@ -219,42 +278,53 @@ class Entidad{
         arrayY.push(random);
     }
     for (i = 0; i < 7; i++){
-        let random = randomInc(1,2);
+        let random = randomInc(1,3);
         if(random == 1){
-            arrayBloques.push(new Entidad(1, 50, arrayX[i], arrayY[i], "tierra.jpg", 0, i));
+            arrayEntidad.push(new Entidad(1, 50, arrayX[i], arrayY[i], "tierra.webp", 0, i));
         }
         else if(random == 2){
-            arrayBloques.push(new Entidad(3, 100, arrayX[i], arrayY[i], "piedra.jpg", 0, i));
+            arrayEntidad.push(new Entidad(3, 100, arrayX[i], arrayY[i], "piedra.webp", 0, i));
+        }
+        else if(random == 3){
+            arrayEntidad.push(new Entidad(2, 200, arrayX[i], arrayY[i], "cuca.png", 1, i));
         }
         let elementoBloque =`
-        <img src="img/${arrayBloques[i].imagen}" class="img" id="b${arrayBloques[i].id}" style="left: ${(arrayBloques[i].x * 60) + 135}px; top: ${(arrayBloques[i].y * 60) + 35}px;">`;
+        <img src="img/${arrayEntidad[i].imagen}" class="img" id="b${arrayEntidad[i].num}" style="left: ${(arrayEntidad[i].x * 60) + 300}px; top: ${(arrayEntidad[i].y * 60) + 35}px;">`;
         bloques.innerHTML += elementoBloque;
     }
-    console.log(arrayBloques)
 //Evento recargar storage
     let recargar = document.getElementById("recargar");
     function reiniciarStorage(){
         localStorage.clear();
     }
     recargar.addEventListener("click", reiniciarStorage);
-//Evento para moverse hacia arriba
-    let btnU = document.getElementById("btnU");
-    btnU.addEventListener("click", u);
-//Evento para moverse hacia abajo
-    let btnD = document.getElementById("btnD");
-    btnD.addEventListener("click", d);
-//Evento para moverse hacia la izquierda
-    let btnL = document.getElementById("btnL");
-    btnL.addEventListener("click", l);
-//Evento para moverse hacia la derecha
-    let btnR = document.getElementById("btnR");
-    btnR.addEventListener("click", r);
+//Evento para moverse con las flechas del teclado
+    function tecla(e){
+        let event = e ? e:event
+        let key = window.Event ? event.which : event.keyCode;
+        if(key == 37){
+            l();
+        }
+        else if(key == 38){
+            u();
+        }
+        else if(key == 39){
+            r();
+        }
+        else if(key == 40){
+            d();
+        }
+    }
+    let flechas = document.getElementById("flechas");
+    flechas.addEventListener("keyup", tecla)
 //Evento para mejorar la pala
     function mejorarPala(){
         if (puntos >= 600){
             mejorar.remove();
             palaMejorada = true;
             localStorage.setItem("palaMejorada", palaMejorada);
+            hud.innerHTML =`
+            <img src="img/hud2_${vidaPersonaje}.webp">`
         }
     }
     mejorar.addEventListener("click", mejorarPala);
